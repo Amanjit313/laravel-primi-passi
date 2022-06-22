9feb10d34dcd7135b1db03bc77a47dca17dbb646
Amanjit313 <amanjitsingh313@gmail.com>
2022-06-22 15:28:33 +0200

    passato un parametro dinamico

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <div>Hello World</div>
+    <div>{{$saluto}}</div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $laravel = [
+        'saluto' => 'Ciao! Benvenuto nel mondo di Laravel!'
+    ];
+
+    return view('home', $laravel);
 });
